13c26f53285bb54e93d9852c7479c7495d150382
Name Surname <vahan.balyan.44@gmail.com>
2023-09-27 21:11:50 +0400

    bootstrap

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,48 +1,30 @@
 import "./App.css"
-import Registerpage from "../registerpage/registerpage"
-import UserPage from "../UserPage/UserPage"
-import NumBoard from "../NumBoard/NumBoard"
-import {useState} from "react"
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import FormControl from 'react-bootstrap/FormControl';
+import { Button } from "react-bootstrap";
 const App = () => {
-    
-
-    const [userData, setUserData] = useState({username: "", email: "", password:""})
-
-    const [flag, setFlag] = useState(false)
-
-    const handleRegistration = (data) => {
-        if(Object.values(data).every(el => el !== "")){
-            setFlag(true)
-            setUserData(data)
-            localStorage.setItem('userData', JSON.stringify(data))
-            localStorage.setItem("flag", true)
-
-        }
-      else {
-        setFlag(false)
-        localStorage.setItem("flag", false)
-    }
-    }
-
-    const logOut = () => {
-        localStorage.setItem('flag', false)
-        setFlag(false)
-    }
-
-    
-        let data = JSON.parse(localStorage.getItem('userData'))
-        let result = !JSON.parse(localStorage.getItem("flag")) ? 
-        <Registerpage handleRegistration= {handleRegistration}/> :
-        <UserPage username = {data.username}
-        logOut = {logOut}
-         email = {(JSON.parse(localStorage.getItem('userData'))).email} 
-         password = {(JSON.parse(localStorage.getItem('userData'))).password}/>
-
-        return (
-           result
-        )
-    
+    return (<>
+        <Navbar expand="lg" className="bg-dark">
+      <Container >
+        <Navbar.Brand className = "text-light" href="#home">Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className = "text-light" href="#">Home</Nav.Link>
+            <Nav.Link className = "text-light" href="#">Obaut</Nav.Link>
+            <Container className="d-flex align-items-end justify-content-end col-10 ">
+            <FormControl type="text" placeholder="Search" />
+            <Button className="col-3">Log in</Button>
+            <Button className="col-3">Regiser</Button>
+            </Container>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
+    )
 }
 
 export default App;
